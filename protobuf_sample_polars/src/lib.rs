778ab1805e3d6828f0d2127336fef42fec489715
jmunar <jmunarriz@gmx.com>
2025-08-21 08:21:48 +0200

    [FEAT-5] UserExtractable trait implementation

--- protobuf_sample_polars/src/lib.rs
+++ protobuf_sample_polars/src/lib.rs
@@ -1,6 +1,6 @@
 use polars_core::prelude::{
-    polars_err, BinaryType, BooleanChunked, BooleanType, ChunkedArray, CompatLevel, DataType, Field, Float64Type,
-    Int64Type, PolarsError, PolarsNumericType, PolarsResult, Series, StringChunked, StringType,
+    polars_err, BinaryType, BooleanType, ChunkedArray, CompatLevel, DataType, Field, Float64Type,
+    Int64Type, PolarsError, PolarsResult, Series, StringType,
 };
 use polars_plan::dsl::FieldsMapper;
 use prost::Message;
@@ -38,98 +38,56 @@ fn extract_output(input_fields: &[Field], kwargs: ExtractKwargs) -> PolarsResult
     FieldsMapper::new(input_fields).with_dtype(data_type)
 }
 
-fn user_extract_typed<TP>(
-    ca: &ChunkedArray<BinaryType>,
-    path: &str,
-    option: bool,
-) -> Result<ChunkedArray<TP>, PolarsError>
-where
-    TP: PolarsNumericType,
-    TP::OwnedPhysical: FromValue<structpath::Value>,
-    Option<TP::OwnedPhysical>: FromValue<structpath::Value>,
-{
-    let chunk_out: Result<ChunkedArray<TP>, PolarsError> = ca
-        .into_iter()
-        .map(|opt_bytes| {
-            match opt_bytes {
-                Some(bytes) => {
+/// Trait for types that can be extracted from protobuf User messages
+trait UserExtractable {
+    type ChunkedArrayType;
 
-                    let user = sample::User::decode(bytes)
-                        .map_err(|e| PolarsError::ComputeError(e.to_string().into()))?;
-                    let value = user.get_value_safe(path)
-                        .map_err(|e| PolarsError::ComputeError(e.to_string().into()))?;
-                    
-                    if option {
-                        Ok(Option::<TP::OwnedPhysical>::from_value(value))
-                    } else {
-                        Ok(Some(TP::OwnedPhysical::from_value(value)))
-                    }
-                }
-                None => Ok(None)
-            }
-        })
-        .collect();
-
-    chunk_out
+    fn extract_from_user_chunked_array(
+        ca: &ChunkedArray<BinaryType>,
+        path: &str,
+        option: bool,
+    ) -> Result<Self::ChunkedArrayType, PolarsError>;
 }
 
-fn user_extract_string(
-    ca: &ChunkedArray<BinaryType>,
-    path: &str,
-    option: bool,
-) -> Result<ChunkedArray<StringType>, PolarsError> {
-    let values: Result<Vec<Option<String>>, PolarsError> = ca
-        .into_iter()
-        .map(|opt_bytes| {
-            match opt_bytes {
-                Some(bytes) => {
-                    let user = sample::User::decode(bytes)
-                        .map_err(|e| PolarsError::ComputeError(e.to_string().into()))?;
-                    let value = user.get_value_safe(path)
-                        .map_err(|e| PolarsError::ComputeError(e.to_string().into()))?;
+/// Macro to generate UserExtractable implementations
+macro_rules! impl_user_extractable {
+    ($polars_type:ty, $rust_type:ty) => {
+        impl UserExtractable for $polars_type {
+            type ChunkedArrayType = ChunkedArray<$polars_type>;
 
-                    if option {
-                        Ok(Option::<String>::from_value(value))
-                    } else {
-                        Ok(Some(String::from_value(value)))
-                    }
-                }
-                None => Ok(None)
+            fn extract_from_user_chunked_array(
+                ca: &ChunkedArray<BinaryType>,
+                path: &str,
+                option: bool,
+            ) -> Result<Self::ChunkedArrayType, PolarsError> {
+                ca.into_iter()
+                    .map(|opt_bytes| {
+                        match opt_bytes {
+                            Some(bytes) => {
+                                let user = sample::User::decode(bytes)
+                                    .map_err(|e| PolarsError::ComputeError(e.to_string().into()))?;
+                                let value = user.get_value_safe(path)
+                                    .map_err(|e| PolarsError::ComputeError(e.to_string().into()))?;
+                                
+                                if option {
+                                    Ok(Option::<$rust_type>::from_value(value))
+                                } else {
+                                    Ok(Some(<$rust_type>::from_value(value)))
+                                }
+                            }
+                            None => Ok(None)
+                        }
+                    })
+                    .collect()
             }
-        })
-        .collect();
-    
-    values.map(|v| v.into_iter().collect::<StringChunked>())
+        }
+    };
 }
 
-fn user_extract_bool(
-    ca: &ChunkedArray<BinaryType>,
-    path: &str,
-    option: bool,
-) -> Result<ChunkedArray<BooleanType>, PolarsError> {
-    let values: Result<Vec<Option<bool>>, PolarsError> = ca
-        .into_iter()
-        .map(|opt_bytes| {
-            match opt_bytes {
-                Some(bytes) => {
-                    let user = sample::User::decode(bytes)
-                        .map_err(|e| PolarsError::ComputeError(e.to_string().into()))?;
-                    let value = user.get_value_safe(path)
-                        .map_err(|e| PolarsError::ComputeError(e.to_string().into()))?;
-                    
-                    if option {
-                        Ok(Option::<bool>::from_value(value))
-                    } else {
-                        Ok(Some(bool::from_value(value)))
-                    }
-                }
-                None => Ok(None)
-            }
-        })
-        .collect();
-    
-    values.map(|v| v.into_iter().collect::<BooleanChunked>())
-}
+impl_user_extractable!(Int64Type, i64);
+impl_user_extractable!(Float64Type, f64);
+impl_user_extractable!(StringType, String);
+impl_user_extractable!(BooleanType, bool);
 
 #[polars_expr(output_type_func_with_kwargs=extract_output)]
 fn user_extract(inputs: &[Series], kwargs: ExtractKwargs) -> PolarsResult<Series> {
@@ -138,21 +96,21 @@ fn user_extract(inputs: &[Series], kwargs: ExtractKwargs) -> PolarsResult<Series
     let path_type = sample::User::get_type_safe(path).unwrap();
 
     match path_type {
-        FieldType::String => user_extract_string(ca, path, false).map(|c| c.into_series()),
+        FieldType::String => StringType::extract_from_user_chunked_array(ca, path, false).map(|c| c.into_series()),
         FieldType::Option(inner_type) if matches!(*inner_type, FieldType::String) => {
-            user_extract_string(ca, path, true).map(|c| c.into_series())
+            StringType::extract_from_user_chunked_array(ca, path, true).map(|c| c.into_series())
         }
-        FieldType::Integer => user_extract_typed::<Int64Type>(ca, path, false).map(|c| c.into_series()),
+        FieldType::Integer => Int64Type::extract_from_user_chunked_array(ca, path, false).map(|c| c.into_series()),
         FieldType::Option(inner_type) if matches!(*inner_type, FieldType::Integer) => {
-            user_extract_typed::<Int64Type>(ca, path, true).map(|c| c.into_series())
+            Int64Type::extract_from_user_chunked_array(ca, path, true).map(|c| c.into_series())
         }
-        FieldType::Float => user_extract_typed::<Float64Type>(ca, path, false).map(|c| c.into_series()),
+        FieldType::Float => Float64Type::extract_from_user_chunked_array(ca, path, false).map(|c| c.into_series()),
         FieldType::Option(inner_type) if matches!(*inner_type, FieldType::Float) => {
-            user_extract_typed::<Float64Type>(ca, path, true).map(|c| c.into_series())
+            Float64Type::extract_from_user_chunked_array(ca, path, true).map(|c| c.into_series())
         }
-        FieldType::Boolean => user_extract_bool(ca, path, false).map(|c| c.into_series()),
+        FieldType::Boolean => BooleanType::extract_from_user_chunked_array(ca, path, false).map(|c| c.into_series()),
         FieldType::Option(inner_type) if matches!(*inner_type, FieldType::Boolean) => {
-            user_extract_bool(ca, path, true).map(|c| c.into_series())
+            BooleanType::extract_from_user_chunked_array(ca, path, true).map(|c| c.into_series())
         }
         _ => panic!("Unsupported type: {:?}", path_type),
     }
